3ba4e3ccef9425d5584e3316002e081eca81a2c9
samueldelorenzi <samueldelorenziribeiro@gmail.com>
2025-06-11 22:12:32 -0300

    migrations

--- migrations/Version20250612012424.php
+++ migrations/Version20250612012424.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20250611002358 extends AbstractMigration
+final class Version20250612012424 extends AbstractMigration
 {
     public function getDescription(): string
     {
